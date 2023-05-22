ce79a65c67fcc6f99260435dc58a6723d5ccc388
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-22 22:44:04 +0300

    Добавила папки front backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,6 @@ const userRouter = require('./users');
 const cardsRouter = require('./cards');
 const { login, createUser } = require('../controllers/users');
 const { loginJoi, createUserJoi } = require('../middlewares/celebrate');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const NotFoundError = require('../errors/notFoundError');
 
@@ -14,15 +13,10 @@ const authMiddleware = require('../middlewares/auth');
 router.post('/signin', loginJoi, login);
 router.post('/signup', createUserJoi, createUser);
 
-app.use(requestLogger); // подключаем логгер запросов
 router.use(authMiddleware);
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
-
-app.use(errorLogger); // подключаем логгер ошибок
-
-router.use(errors({ message: 'Ошибка валидации данных!' }));
-
 router.use('/*', (req, res, next) => next(new NotFoundError('404: Not Found.')));
+router.use(errors({ message: 'Ошибка валидации данных!' }));
 
 module.exports = router;
